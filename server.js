6b137cb1af1021dac4a0c4b37e6b68740267be61
fredrickUdom <fredrickudom@gmail.com>
2024-03-30 20:51:08 +0100

    hot fix

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 const dbConnection = require('./databaseConnection');
-const router = require('./router/roomsRouter');
+const roomsRouter = require('./router/roomsRouter');
 
 
 const app = express();
 app.use(express.json());
-app.use('/api/rooms', router);
+app.use('/api/rooms', roomsRouter);
 const port = process.env.PORT;
 dbConnection.Connection;
 app.listen(port, () => console.log(`server listening on ${port}`));
