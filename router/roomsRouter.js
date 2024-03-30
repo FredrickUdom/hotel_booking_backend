37f641eb1645a252ea6efc17cb5ad83efea5f5d0
fredrickUdom <fredrickudom@gmail.com>
2024-03-30 17:17:14 +0100

    quick fix

--- router/roomsRouter.js
+++ router/roomsRouter.js
@@ -4,10 +4,9 @@ const Room = require('../model/room');
 
 router.get('/getAllRooms', async(req, res) => {
     try {
-        const room = await Room.find({
-            
-        });
-        return res.json({room});
+        const room = await Room.find({});
+        res.send(room)
+        // return res.json({room});
     } catch (error) {
         return res.json({message:error}).status(400)
     }
